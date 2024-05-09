3b8c004da5b0d056ebdaca7d0440e447fb442426
jonah ominyi <jonahjohnzon@gmail.com>
2024-05-09 09:44:32 +0000

    cache

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+'''Basic dictionary
+'''
+
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    '''A class `BasicCache` that inherits from `BaseCaching`
+    '''
+
+    def put(self, key, item):
+        '''assign to the dictionary `self.cache_data`
+        '''
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        '''return value in `self.cache_data` linked `key`
+        '''
+
+        return self.cache_data.get(key, None)
